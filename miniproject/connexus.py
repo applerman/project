2c609ca63dfbaa53d22a80bf7bf1887b63aa9dee
chunheng <chunheng@utexas.edu>
2015-09-28 17:06:42 -0500

    haha, I forgot to push

--- miniproject/connexus.py
+++ miniproject/connexus.py
@@ -1,15 +1,17 @@
+import datetime 
 import urllib
 import webapp2
 from google.appengine.api import images
+from google.appengine.api import search
 from google.appengine.api import users
 from google.appengine.ext import ndb
-from google.appengine.api import search
 
 
 HEAD = """\
 <html>
   <body>
-    <h3>Connexus.us Welcome, %s! (<a href="%s">sign out</a>)</h3>
+    <h3>Connex.us</h3>
+    Welcome, %s! (<a href="%s">sign out</a>)<br>
     <a href="manage">Manage</a> |
     <a href="create">Create</a> |
     <a href="view">View</a> |
@@ -19,7 +21,7 @@ HEAD = """\
     <hr>
 """
 
-TAIL = """
+TAIL = """\
   </body>
 </html>
 """
@@ -29,15 +31,16 @@ showSearchResults = False
 ##############
 # Data Model
 ##############
-class Picture(ndb.Model):
+class Picture(ndb.Model):  
   stream_id = ndb.StringProperty(indexed=True)
+  created_date = ndb.DateTimeProperty(auto_now_add=True)
   image = ndb.BlobProperty()
   comment = ndb.StringProperty()
 
 class Stream(ndb.Model):
   creator_id = ndb.StringProperty(indexed=True)
   name = ndb.StringProperty(indexed=True)
-  last_updated_date = ndb.DateTimeProperty()
+  last_updated_date = ndb.DateTimeProperty(auto_now_add=True)
   created_date = ndb.DateTimeProperty(auto_now_add=True)
   num_pictures = ndb.IntegerProperty()
   num_views = ndb.IntegerProperty()
@@ -69,6 +72,7 @@ class Login(webapp2.RequestHandler):
 
 class Manage(webapp2.RequestHandler):
   def post(self):
+    if self.request.get('create'):
       # When Create Page submits, executes the following code.
       stream = Stream()
       stream.creator_id = users.get_current_user().user_id()
@@ -89,33 +93,45 @@ class Manage(webapp2.RequestHandler):
 
       # Put in Index
       try:
-        index = search.Index(name="myIndex")
+        index = search.Index(name='myIndex')
         index.put(currentDocument)
       except search.Error:
         pass #print "Fail in putting in the Index" #
 
       self.redirect('/manage')
+  #TODO
+
 
   def get(self):
+    # TODO make it a table?
     user = users.get_current_user()
     PAGE = HEAD % (user.nickname(), users.create_logout_url('/'))
-    PAGE += "<b>Streams I own</b><br>"
+    PAGE += "<b>Streams I own</b><br>Name | Last New Picture | Number of Pictures <br>"
     streams_i_own = Stream.query(Stream.creator_id ==
                                  users.get_current_user().user_id()).order(-Stream.created_date)
     for stream in streams_i_own:
-      PAGE += "|" + stream.name
+      PAGE += "<a href=/view?%s>%s</a> |" % (urllib.urlencode({'stream': stream.name}), stream.name)
+      PAGE += str(stream.last_updated_date.date()) + '|'
+      PAGE += str(stream.num_pictures) + '<br>'
+
     PAGE += "<br>"
-    PAGE += "<b>Streams I subscribe to</b>"
-    user = User.query(User.identity == users.get_current_user().user_id())
-    # TODO handle subscription
-      
+    PAGE += "<b>Streams I subscribe to</b><br>Name | Last New Picture | Number of Pictures | Views <br>"
+    cur_user = User.query(User.identity == users.get_current_user().user_id()).fetch(1)
+    if cur_user:
+      for stream_name in cur_user[0].subscriptions:
+        stream = Stream.query(Stream.name == stream_name).fetch(1)[0]
+        PAGE += "<a href=/view?%s>%s</a> |" % (urllib.urlencode({'stream': stream.name}), stream.name)
+        PAGE += str(stream.last_updated_date.date()) + '|'
+        PAGE += str(num_pictures) + '<br>'
+        PAGE += str(num_views) + '<br>'
+
+    # TODO handle delete
     PAGE += "</body></html>"
     self.response.write(PAGE)
 
 
 class Create(webapp2.RequestHandler):
   def get(self):
-
     user = users.get_current_user()
     PAGE = HEAD % (user.nickname(), users.create_logout_url('/'))
     PAGE += """\
@@ -129,7 +145,7 @@ class Create(webapp2.RequestHandler):
       <div><textarea name="tag" rows="3" cols="60"></textarea></div>
       <div>URL to cover image (Can be empty)</div>
       <div><input value="" name="cover_img_url"></div>
-      <div><input type="submit" value="Create Streams"></div>
+      <div><input type="submit" name="create"></div>
     </form>
     """ + TAIL
     self.response.write(PAGE)
@@ -137,32 +153,59 @@ class Create(webapp2.RequestHandler):
 class View(webapp2.RequestHandler):
   def post(self):
     # TODO store image, and update ...
-    pass
+    picture = Picture()
+    stream_name = self.request.get('stream')
+    picture.stream_id = stream_name
+    picture.image = self.request.get('img')
+    picture.comment = self.request.get('comment')
+    picture.put()
+
+    stream = Stream.query(Stream.name == stream_name).fetch(1)[0]
+    stream.last_updated_date = datetime.datetime.now()
+    stream.num_pictures += 1
+    stream.put()
+
+    self.redirect('/view?%s' % urllib.urlencode({'stream': stream_name}))
+
 
   def get(self):
     stream_name = self.request.get('stream')
-    # TODO find stream using stream_name, and print pictures
-    user = users.get_current_user()
-    PAGE = HEAD % (user.nickname(), users.create_logout_url('/'))
-    PAGE += """
-      <b>%s</b>
-      <form action="/view?%s" enctype="multipart/form-data" method="post">
-        <div><textarea name="comment" rows="3" cols="60">comment</textarea></div>
-        <div><label>image:</label></div>
-        <div><input type="file" name="img"/></div>
-        <div><input type="submit" value="Upload"></div>
-      </form>
-      """ % (stream_name, urllib.urlencode({'stream': stream_name}))
-    PAGE += TAIL
+    if stream_name:
+      stream = Stream.query(Stream.name == stream_name).fetch(1)[0]
+      stream.num_views += 1
+      stream.put()
 
-    self.response.write(PAGE)
+      user = users.get_current_user()
+      PAGE = HEAD % (user.nickname(), users.create_logout_url('/'))
 
-class ViewAll(webapp2.RequestHandler):
-  def get(self):
-    user = users.get_current_user()
-    PAGE = HEAD % (user.nickname(), users.create_logout_url('/'))
-    PAGE += "<b>View All Streams</b>" + TAIL
-    self.response.write(PAGE)
+      PAGE += "<b>%s</b><br>" % stream_name
+
+      pictures = Picture.query(Picture.stream_id == stream_name).order(Picture.created_date)
+      for pic in pictures:
+        PAGE += ('<img src="/img?img_id=%s"></img>' % pic.key.urlsafe())
+
+      PAGE += """\
+    <form action="/view?%s" enctype="multipart/form-data" method="post">
+      <div><textarea name="comment" rows="3" cols="60">comment</textarea></div>
+      <div><label>image:</label></div>
+      <div><input type="file" name="img"/></div>
+      <div><input type="submit" name="upload"></div>
+    </form> """ % (urllib.urlencode({'stream': stream_name}))
+      PAGE += TAIL
+
+      self.response.write(PAGE)
+
+    else:
+      user = users.get_current_user()
+      PAGE = HEAD % (user.nickname(), users.create_logout_url('/'))
+      PAGE += "<b>View All Streams</b><br>"
+      streams = Stream.query()
+      for stream in streams:
+        PAGE += "<a href=/view?%s>%s</a><br>" % (urllib.urlencode({'stream': stream.name}), stream.name)
+        PAGE += ('<img src="%s"></img><br>' % stream.cover_img_url)
+
+      PAGE += TAIL
+      self.response.write(PAGE)
 
 class Search(webapp2.RequestHandler):
   def post(self):
@@ -227,15 +270,25 @@ class Social(webapp2.RequestHandler):
     PAGE += "<b>Social</b>" + TAIL
     self.response.write(PAGE)
 
+class Image(webapp2.RequestHandler):
+  def get(self):
+    picture_key = ndb.Key(urlsafe=self.request.get('img_id'))
+    picture = picture_key.get()
+    if picture.image:
+      self.response.headers['Content-Type'] = 'image/gif'
+      self.response.out.write(images.resize(picture.image, height=64, allow_stretch=False))
+    else:
+      self.response.out.write('No image')
+
 
 app = webapp2.WSGIApplication([
   ('/', Login),
   ('/manage', Manage),
   ('/create', Create),
-  ('/view', ViewAll),
-  ('/viewall', ViewAll),
+  ('/view', View),
   ('/search', Search),
   ('/trending', Trending),
   ('/social', Social),
-  ('/error', Error)
+  ('/error', Error),
+  ('/img', Image)
 ], debug=True)
