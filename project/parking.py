c1866311429e6c4845635d1023fc6c0a424829c8
chunheng <chunheng@utexas.edu>
2015-12-09 21:53:07 -0600

    server

--- project/parking.py
+++ project/parking.py
@@ -1,43 +1,70 @@
 import cgi
+import json
 import urllib
+import webapp2
 
+from google.appengine.api import images
 from google.appengine.api import users
 from google.appengine.ext import ndb
-
-import webapp2
 
 ##############
 # Data Model
 ##############
-class Picture(ndb.Model):  
-  stream_id = ndb.StringProperty(indexed=True)
-  created_date = ndb.DateTimeProperty(auto_now_add=True)
-  date = ndb.DateProperty(auto_now_add=True)
-  image = ndb.BlobProperty()
-  caption = ndb.StringProperty()
+class Parking(ndb.Model):
+  user_email = ndb.StringProperty(indexed=True)
   geo = ndb.GeoPtProperty()
-
-class Stream(ndb.Model):
-  creator_id = ndb.StringProperty(indexed=True)
-  name = ndb.StringProperty(indexed=True)
-  last_updated_date = ndb.DateTimeProperty(auto_now_add=True)
-  created_date = ndb.DateTimeProperty(auto_now_add=True)
-  num_pictures = ndb.IntegerProperty()
-  num_views = ndb.IntegerProperty()
-  tag = ndb.StringProperty(repeated=True)
-  cover_img_url = ndb.StringProperty()
-  time_stamp = ndb.DateTimeProperty(repeated=True)
+  image = ndb.BlobProperty()
+  description = ndb.StringProperty()
+  created_time = ndb.DateTimeProperty(auto_now_add=True)
+  done_parking = ndb.BooleanProperty()
 
 class User(ndb.Model):
-  identity = ndb.StringProperty(indexed=True)
-  email = ndb.StringProperty(indexed=True)
-  subscriptions = ndb.StringProperty(repeated=True)
-  trending_report = ndb.StringProperty(indexed=True)
+  user_email = ndb.StringProperty(indexed=True)
+  car = ndb.StringProperty()
 
 ##############
 # Handlers
 ##############
+class Park(webapp2.RequestHandler):
+  def get(self):
+    if self.request.get('viewparking'):
+      user_email = self.request.get('user_email')
+      parkings = Parking.query(Parking.user_email == user_email).order(-Parking.created_time).fetch(16)
+
+      parkingLat = []
+      parkingLon = []
+      parkingDone = []
+      parkingTime = []
+
+      for parking in parkings:
+        parkingLat.append(parking.geo.lat)
+        parkingLon.append(parking.geo.lon)
+        parkingDone.append(parking.done_parking)
+        parkingTime.append(parking.created_time)
+
+      dictPassed = {'parkingLat':parkingLat, 'parkingLon':parkingLon,
+                    'parkingDone':parkingDone, 'parkingTime':parkingTime}
+
+      jsonObj = json.dumps(dictPassed, sort_keys=True,indent=4, separators=(',', ': '))
+      self.response.write(jsonObj)
+
+    elif self.request.get('viewuser'):
+      user_email = self.request.get('user_email')
+
+  def post(self):
+    lat = self.request.get('lat')
+    lon = self.request.get('lon')
+
+    if lat and lon:
+      parking = Parking()
+      parking.user_email = self.request.get('user_email')
+      parking.geo = ndb.GeoPt(float(lat), float(lon))
+      image = self.request.get('image')
+      picture.image = images.resize(image, height=1080, allow_stretch=False)
+      parking.description = self.request.get('description')
+      parking.done_parking = False
+      parking.put()
 
 app = webapp2.WSGIApplication([
-  ('/', Login)
+  ('/park', Park)
 ], debug=True)
