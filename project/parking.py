02867c3f1e4d087e8a1d9313229e0d87162c0fe4
chunheng <chunheng@utexas.edu>
2015-12-10 22:03:15 -0600

    bug

--- project/parking.py
+++ project/parking.py
@@ -17,6 +17,7 @@ class Parking(ndb.Model):
   description = ndb.StringProperty()
   created_time = ndb.DateTimeProperty(auto_now_add=True)
   done_parking = ndb.BooleanProperty()
+  shared_parking = ndb.BooleanProperty()
 
 class User(ndb.Model):
   user_email = ndb.StringProperty(indexed=True)
@@ -29,7 +30,9 @@ class Park(webapp2.RequestHandler):
   def get(self):
     if self.request.get('viewparking'):
       user_email = self.request.get('user_email')
-      parkings = Parking.query(Parking.user_email == user_email).order(-Parking.created_time).fetch(16)
+      shared_parking = bool(self.request.get('shared_parking'))
+      parkings = Parking.query(Parking.user_email == user_email and
+                               Parking.shared_parking == shared_parking).order(-Parking.created_time).fetch(16)
 
       parkingLat = []
       parkingLon = []
@@ -60,9 +63,10 @@ class Park(webapp2.RequestHandler):
       parking.user_email = self.request.get('user_email')
       parking.geo = ndb.GeoPt(float(lat), float(lon))
       image = self.request.get('image')
-      picture.image = images.resize(image, height=1080, allow_stretch=False)
+      parking.image = images.resize(image, height=1080, allow_stretch=False)
       parking.description = self.request.get('description')
       parking.done_parking = False
+      parking.shared_parking = bool(self.request.get('shared_parking'))
       parking.put()
 
 app = webapp2.WSGIApplication([
