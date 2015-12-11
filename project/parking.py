be5973f48fb1ac917969e4f1a566c77bc2598815
chunheng <chunheng@utexas.edu>
2015-12-11 09:36:13 -0600

    server side update

--- project/parking.py
+++ project/parking.py
@@ -31,22 +31,26 @@ class Park(webapp2.RequestHandler):
     if self.request.get('viewparking'):
       user_email = self.request.get('user_email')
       shared_parking = bool(self.request.get('shared_parking'))
+      recent_one = bool(self.request.get('recent_one'))
+      number = 16
+      if recent_one:
+         number = 1
       parkings = Parking.query(Parking.user_email == user_email and
-                               Parking.shared_parking == shared_parking).order(-Parking.created_time).fetch(16)
+                               Parking.shared_parking == shared_parking).order(-Parking.created_time).fetch(number)
 
       parkingLat = []
       parkingLon = []
       parkingDone = []
-      parkingTime = []
+      parkingImgURL = []
 
       for parking in parkings:
         parkingLat.append(parking.geo.lat)
         parkingLon.append(parking.geo.lon)
         parkingDone.append(parking.done_parking)
-        parkingTime.append(parking.created_time)
+        parkingImgURL.append("http://parkingrighthere.appspot.com/img?img_id=%s" % parking.key.urlsafe())
 
       dictPassed = {'parkingLat':parkingLat, 'parkingLon':parkingLon,
-                    'parkingDone':parkingDone, 'parkingTime':parkingTime}
+                    'parkingDone':parkingDone, 'parkingImgURL':parkingImgURL}
 
       jsonObj = json.dumps(dictPassed, sort_keys=True,indent=4, separators=(',', ': '))
       self.response.write(jsonObj)
@@ -63,12 +67,25 @@ class Park(webapp2.RequestHandler):
       parking.user_email = self.request.get('user_email')
       parking.geo = ndb.GeoPt(float(lat), float(lon))
       image = self.request.get('image')
-      parking.image = images.resize(image, height=1080, allow_stretch=False)
+      if image:
+        parking.image = images.resize(image, height=1080, allow_stretch=False)
       parking.description = self.request.get('description')
       parking.done_parking = False
       parking.shared_parking = bool(self.request.get('shared_parking'))
       parking.put()
 
+class Image(webapp2.RequestHandler):
+  def get(self):
+    park_key = ndb.Key(urlsafe=self.request.get('img_id'))
+    park = park_key.get()
+    if park.image:
+      self.response.headers['Content-Type'] = 'image/gif'
+      self.response.out.write(park.image)
+    else:
+      self.response.out.write('No image')
+
+
 app = webapp2.WSGIApplication([
-  ('/park', Park)
+  ('/park', Park),
+  ('/img', Image)
 ], debug=True)
